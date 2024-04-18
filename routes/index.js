0e0e0a7868aad3692c60272eb00ddc6a683ffe8e
datle-dev <dle.dev@outlook.com>
2024-04-18 05:24:23 -0400

    Add message data to pass down as locals
    Add data in index route to be passed to home page on render and
    use EJS syntax to loop through messages and display each data field.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  },
+  {
+    text: "Howdy!",
+    user: "Jeff",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 module.exports = router;
