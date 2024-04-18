b39afad21b6a1efd4d199e10dacb787297319cc9
datle-dev <dle.dev@outlook.com>
2024-04-18 06:15:30 -0400

    Set new form post to update app.locals

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const bodyParser = require('body-parser');
 
 const indexRouter = require("./routes/index");
-const newRouter = require('./routes/new');
+const newRouter = require("./routes/new");
 
 const app = express();
 
@@ -31,13 +30,22 @@ app.locals.messages = [
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
-app.use('/new', newRouter);
+app.use("/new", newRouter);
+
+app.post("/new", function (req, res, next) {
+  req.app.locals.messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
 
 app.listen(8080, () => {
-    console.log(`Server started on port ${8080}`)
+  console.log(`Server started on port ${8080}`);
 });
 
 module.exports = app;
