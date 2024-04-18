f5bc9263d15f3fb4bb5986241c97c6febb77aa44
datle-dev <dle.dev@outlook.com>
2024-04-18 05:27:00 -0400

    Replace boilerplate with new message form
    Create new view and set up new router for a new message form

---
+++ app.js
@@ -0,0 +1,22 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const newRouter = require('./routes/new');
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+app.use('/new', newRouter);
+
+app.listen(8080, () => {
+    console.log(`Server started on port ${8080}`)
+});
+
+module.exports = app;
