f97e87f69ff96ded01a077ab50733f87676aa7a1
datle-dev <dle.dev@outlook.com>
2024-04-18 05:48:47 -0400

    Remove body-parser package and use express built-in instead

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 
 app.use("/", indexRouter);
 app.use('/new', newRouter);
